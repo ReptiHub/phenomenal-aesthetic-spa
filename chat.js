11fb2b938cbdf8eb79b21786f42448734a53eb7a
Reptify Media <seggtan@gmail.com>
2026-08-15 12:31:40 -0700

    Bump asset version stamp

--- chat.js
+++ chat.js
@@ -199,7 +199,7 @@
     document.body.classList.add('chat-open');
     launcher.setAttribute('aria-expanded','true');
     if (!FAQ){
-      fetch('faq.json?v=448a8a0').then(function(r){ return r.json(); }).then(function(d){
+      fetch('faq.json?v=26ab7ac').then(function(r){ return r.json(); }).then(function(d){
         FAQ = d;
         if (!started) start();
       }).catch(function(){
